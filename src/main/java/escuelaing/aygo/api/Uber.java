faac46996ef7b1c7827cbc5fb5e899a473274b96
Sebastian <1144105003@u.icesi.edu.co>
2024-11-17 17:54:58 -0500

    Spring initializater

---
+++ src/main/java/escuelaing/aygo/api/Uber.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package escuelaing.aygo.api;
+
+/**
+ *
+ * @author Sebastian
+ */
+public class Uber {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
